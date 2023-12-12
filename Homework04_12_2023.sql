2c9089fc1e5699f71790e5ee5c67c0408ae54a86
LenaZ2045 <131924446+LenaZ2045@users.noreply.github.com>
2023-12-11 20:15:26 -0500

    Update Homework04_12_2023.sql
    Corrected Format

--- Homework04_12_2023.sql
+++ Homework04_12_2023.sql
@@ -2,16 +2,16 @@
 
 Video Hosting (Youtube) CRUD is defined as C-create R-read U-update and D-delete
 
-- Uploading Shorts (create)
-- Looking through comments (read)
-- Changing profile data (update)
-- Deleting a subscriber (delete)
-- Blocking the comment option for videos (update)
-- Search for the required content (read)
-- Subscription to a new user (update)
-- Clearing history of watched videos (delete)
-- Recording a video list for viewing it later (create)
-- Viewing of notification/alerts (read)
+first - Uploading Shorts (create)
+second - Looking through comments (read)
+third - Changing profile data (update)
+fourth - Deleting a subscriber (delete)
+fifth - Blocking the comment option for videos (update)
+sixth - Search for the required content (read)
+seventh - Subscription to a new user (update)
+eighth - Clearing history of watched videos (delete)
+nineth - Recording a video list for viewing it later (create)
+tenth - Viewing of notification/alerts (read)
 
 В рамках БД "песочница" напишите след/запросы:
 
@@ -20,17 +20,17 @@ Video Hosting (Youtube) CRUD is defined as C-create R-read U-update and D-delete
 SELECT ProductName, Price
 FROM Products
 WHERE
-	Price >=20
-    AND
-    Price <= 150
+ Price >=20
+ AND
+ Price <= 150
 
 3. Вывести страны поставщиков, кроме тех, что из USA.
 
 SELECT Country
 FROM Suppliers
 WHERE
-	NOT
-    Country = 'USA'
+ NOT
+ Country = 'USA'
 
 4. Вывести перевозчика с ID 1.
 
@@ -41,29 +41,29 @@ For Shippers:
 SELECT *
 FROM Shippers
 WHERE
-	ShipperID = 1
+ ShipperID = 1
 
 For Suppliers
 
 SELECT *
 FROM Suppliers
 WHERE
-	SupplierID = 1
+ SupplierID = 1
 
 5. Вывести контактные имена клиентов, кроме тех, что из France и USA.
 
 SELECT ContactName, Country
 FROM Customers
 WHERE
-	NOT Country = 'France'
-    AND
-    NOT Country = 'USA'
+ NOT Country = 'France'
+ AND
+ NOT Country = 'USA'
 
 6. Вывести страну, город и адрес всех поставщиков, кроме тех, что из Japan и Brazil.
 
 SELECT SupplierName, Country, City, Address
 FROM Suppliers
 WHERE
-	NOT ( Country = 'Japan'
-    OR
-    Country = 'Brazil')
+ NOT ( Country = 'Japan'
+ OR
+ Country = 'Brazil')
